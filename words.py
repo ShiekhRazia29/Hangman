74ee4e8501f5417b9b9371b34f06728b555e301c
Shiekh Razia <89095507+ShiekhRazia29@users.noreply.github.com>
2021-10-30 19:16:00 +0800

    Hangman

---
+++ words.py
@@ -0,0 +1,23 @@
+import string
+import random
+
+def load_words():
+    """
+    Ye function kaafi jayada words ko load karne mai help karega
+    """
+    #word_list = ["navgurukul", "learning", "kindness"]
+    #return words.txt
+    with open("words.txt","r") as read_word:
+        line=read_word.readline()
+        read_word=line.split()
+        return read_word
+def choose_word():
+    """
+    word_list (list): list of words (strings)
+    ye function ek word randomly return karega
+    """
+    word_list = load_words()
+    secret_word = random.choice(word_list)
+    secret_word = secret_word.lower()
+
+    return secret_word
